46142bb0fdfe34d4da1b1fdb921572f29a93cc3a
Francisco Miguel Suero Nin <franciscosueronin@gmail.com>
2017-01-20 20:59:59 -0400

    La parte 4

---
+++ asignacion_1/parte_4.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/parte4', function(req, res) {
+	var hostArray = req.get('host').split(":");
+	var header = JSON.stringify(req.headers);
+	var responseJson = {
+		"Method" : req.method,
+		"Path" : req.path,
+		"Port" : hostArray[1],
+		"Headers": header
+	}
+	res.send(ResponseJson);
+});
+
+app.listen(8082, function(){
+	console.log('listen 8082 port.');
+})
